584a2ccea916f8347e66f4b85184d2f20561d31c
BRAGGZ9 <brianbinomugisha9@gmail.com>
2022-10-04 07:49:39 +0300

    updates

--- easy_coffee_copy/easy_coffee_copy_1/lib/components/dry_seeds.dart
+++ easy_coffee_copy/easy_coffee_copy_1/lib/components/dry_seeds.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../map.dart';
+import 'farm_tools.dart';
+
 class Dry_seeds extends StatelessWidget {
   // final Product product;
   // const DetailsScreen({key, this.product});
@@ -43,142 +46,71 @@ class finalScreen extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(children: [
         SizedBox(
-          height: size.height,
-          child: Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.all(0),
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(50)),
-                child: Container(
-                  height: 250,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/coffe_type.jpg'),
-                          fit: BoxFit.cover)),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(top: size.height * 0.12),
-                  height: 1000,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 238, 232),
-                      //curving at the top
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50),
-                          topRight: Radius.circular(50))),
-
-                  //content of the card_0n tap ui
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.all(20),
-                          child: (Text('dhggfhgfjhhghgf'))) //code here
-                    ],
-                  )
-
-                  //desighn of the background
+            height: size.height,
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(0),
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(50)),
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/coffe_type.jpg'),
+                            fit: BoxFit.cover)),
                   ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(''),
-                    Text('Dry seeds',
-                        style: Theme.of(context).textTheme.headline4?.copyWith(
-                            color: Colors.brown, fontWeight: FontWeight.bold)),
-                    Row(
-                      children: [
-                        RichText(
-                            text: TextSpan(children: [
-                          TextSpan(
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline4
-                                  ?.copyWith(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold))
-                        ])),
-                      ],
-                    ) //headlining word
-                  ],
                 ),
-              )
+                Container(
+                    margin: EdgeInsets.only(top: size.height * 0.12),
+                    height: 1000,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 238, 232),
+                        //curving at the top
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20))),
+
+                    //content of the card_0n tap ui
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.all(20),
+                              child: (Text('dhggfhgfjhhghgf')))
+                        ]) //code here
+                    //desighn of the background
+                    )
+              ],
+            )
+
+            //desighn of the background
+            ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(''),
+              Text('Dry seeds',
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      color: Colors.brown, fontWeight: FontWeight.bold)),
+              Row(
+                children: [
+                  RichText(
+                      text: TextSpan(children: [
+                    TextSpan(
+                        style: Theme.of(context).textTheme.headline4?.copyWith(
+                            color: Colors.black, fontWeight: FontWeight.bold))
+                  ])),
+                ],
+              ) //headlining word
             ],
           ),
         ),
         DetailContentMenu()
       ]),
-    );
-  }
-}
-
-//copyright code and social media handles
-class DetailContentMenu extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Text(
-            "© All rights reserved. e_coffee Limited",
-            style: TextStyle(
-                fontSize: 15.0,
-                color: Colors.black87,
-                fontWeight: FontWeight.w400,
-                height: 1.50),
-            textAlign: TextAlign.justify,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.facebook,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.whatsapp,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.telegram,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(6.0),
-                child: IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.tiktok,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
